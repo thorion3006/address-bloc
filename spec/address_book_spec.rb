cf55bf976ec7b43d444f6b9c88c79c5bedb4287f
Sajeev Ramasamy <thorion3006@gmail.com>
2017-03-28 15:57:29 +0000

    Assignment 5 Models: Completed

--- spec/address_book_spec.rb
+++ spec/address_book_spec.rb
@@ -28,13 +28,38 @@ RSpec.describe AddressBook do
         end
         
         it "adds the correct information to entries" do
-            book =AddressBook.new
+            book = AddressBook.new
             book.add_entry('Ada Lovelace', '010.012.1815', 'augusta.king@lovelace.com')
             new_entry = book.entries[0]
             
             expect(new_entry.name).to eq('Ada Lovelace')
             expect(new_entry.phone_number).to eq('010.012.1815')
             expect(new_entry.email).to eq('augusta.king@lovelace.com')
+        end
+    end
+    
+    describe "#remove_entry" do
+        it "removes a single entry from the address book" do
+            book = AddressBook.new
+            book.add_entry('Ada Lovelace', '010.012.1815', 'augusta.king@lovelace.com')
+            book.add_entry('Sam Fisher', '020.024.2815', 'sam.fisher@lovelace.com')
+            expect(book.entries.size).to eq(2)
+            
+            book.remove_entry('Ada Lovelace', '010.012.1815', 'augusta.king@lovelace.com')
+            expect(book.entries.size).to eq(1)
+        end
+        
+        it "removes the correct information from the entries" do
+            book = AddressBook.new
+            book.add_entry('Ada Lovelace', '010.012.1815', 'augusta.king@lovelace.com')
+            book.add_entry('Sam Fisher', '020.024.2815', 'sam.fisher@lovelace.com')
+            
+            book.remove_entry('Ada Lovelace', '010.012.1815', 'augusta.king@lovelace.com')
+            entry_left = book.entries[0]
+            
+            expect(entry_left.name).to eq('Sam Fisher')
+            expect(entry_left.phone_number).to eq('020.024.2815')
+            expect(entry_left.email).to eq('sam.fisher@lovelace.com')
         end
     end
 end
