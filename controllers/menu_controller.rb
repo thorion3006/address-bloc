f235ade6f0ec884c7cb926cf7ec18112e09415d5
Sajeev Ramasamy <thorion3006@gmail.com>
2017-03-29 21:42:34 +0530

    Assignment 9 Menu continued: Completed 2

--- controllers/menu_controller.rb
+++ controllers/menu_controller.rb
@@ -10,10 +10,11 @@ class MenuController
     def main_menu
         puts "Main Menu - #{address_book.entries.count} entries"
         puts "1 - View all entries"
-        puts "2 - Create an entry"
-        puts "3 - Search for an entry"
-        puts "4 - Import entries from a CSV"
-        puts "5 - Exit"
+        puts "2 - Delete all entries"
+        puts "3 - Create an entry"
+        puts "4 - Search for an entry"
+        puts "5 - Import entries from a CSV"
+        puts "6 - Exit"
         print "Enter your selection: "
 
         selection = gets.to_i
@@ -25,17 +26,21 @@ class MenuController
                 main_menu
             when 2
                 system "clear"
-                create_entry
+                nuke
                 main_menu
             when 3
                 system "clear"
-                search_entries
+                create_entry
                 main_menu
             when 4
                 system "clear"
-                read_csv
+                search_entries
                 main_menu
             when 5
+                system "clear"
+                read_csv
+                main_menu
+            when 6
                 puts "Good-bye!"
                 exit(0)
             else
@@ -74,11 +79,9 @@ class MenuController
     end
 
     def nuke
-        system "clear"
         puts "Are you sure that you want to destroy the whole book? Type 'y' to continue and 'n' to go back to main menu"
         selection = gets.chomp
         address_book.entries.clear if selection == "y"
-        main_menu
     end
 
     def delete_entry(entry)
